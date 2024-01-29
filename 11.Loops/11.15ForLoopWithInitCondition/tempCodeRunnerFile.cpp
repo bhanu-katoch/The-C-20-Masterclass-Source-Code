d9fdcb492555d02585081ae3fa8acc619511ff9e
bhanu <bhanukatoch445@gmail.com>
2024-01-29 13:59:28 +0530

    gitignore

---
+++ 11.Loops/11.15ForLoopWithInitCondition/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+double multiplier{4};
---
+++ 12.Arrays/12.3SizeOfAnArray/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+int count { std::size(scores)}; // std::size( C++17)
+
+    std::cout << "sizeof(scores) : " << sizeof(scores) << std::endl;
+    std::cout << "sizeof(scores[0]) : " << sizeof(scores[0]) << std::endl;
+    std::cout << "count : " << count << std::endl;
